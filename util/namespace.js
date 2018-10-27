8c413ad0a14be386ce5de343b91e5fd49837f0b3
Adam Palaniuk <apalaniuk@gmail.com>
2018-10-27 00:54:45 -0500

    simpler namespace building move d2l-typography to production dependency

--- util/namespace.js
+++ util/namespace.js
@@ -1,29 +1,6 @@
 'use strict';
 
-(function() {
-	var shape = {
-		D2L: {
-			PolymerBehaviors: {
-				FileViewer: {
-					LangTerms: {}
-				}
-			}
-		}
-	};
-
-	function createNamespace(scope, target) {
-		for (var namespace in scope) {
-			target[namespace] = target[namespace] || {};
-
-			createNamespace(scope[namespace], target[namespace]);
-		}
-	}
-
-	try {
-		if (typeof window.D2L.PolymerBehaviors.FileViewer !== 'object') {
-			throw Error();
-		}
-	} catch {
-		createNamespace(shape, window);
-	}
-})();
+window.D2L = window.D2L || {};
+window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
+window.D2L.PolymerBehaviors.FileViewer = window.D2L.PolymerBehaviors.FileViewer || {};
+window.D2L.PolymerBehaviors.FileViewer.LangTerms = window.D2L.PolymerBehaviors.FileViewer.LangTerms || {};
